7c5d5d592dcbf10d5c6a0225377c2fb1b88d094e
Francis Surroca <fsurroca@microsoft.com>
2022-04-01 12:23:13 -0700

    LruCache impl

---
+++ LRUCache/LruCache.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace LRUCache
+{
+    public class LruCache<T,T2>
+    {
+        private class CacheItem<Tk,Tv> { public Tk k; public Tv v; }
+        private int capacity;
+        private Dictionary<T, LinkedListNode<CacheItem<T,T2>>> d;
+        private LinkedList<CacheItem<T, T2>> ll;
+        /* 
+
+        */
+        public LruCache(int capacity)
+        {
+            this.capacity = capacity;
+            d = new Dictionary<T, LinkedListNode<CacheItem<T,T2>>>();
+            ll = new LinkedList<CacheItem<T,T2>>();
+        }
+
+        public T2 Get(T key)
+        {
+            T2 r = default(T2);
+            if (d.ContainsKey(key))
+            {
+                r = d[key].Value.v;
+                Promote(key);
+            }
+            return r;
+        }
+
+        public void Put(T key, T2 value)
+        {
+            if (d.ContainsKey(key))
+            {
+                Update(key, value);
+                Promote(key);
+            } 
+            else
+            {
+                if (d.Keys.Count == capacity)
+                {
+                    Evict();
+                }
+
+                var lln = new LinkedListNode<CacheItem<T, T2>>(new CacheItem<T,T2> { k=key, v=value } );
+                d.Add(key, lln);
+                ll.AddFirst(lln);
+            }
+        }
+
+        public List<T2> GetLruList()
+        {
+            List<T2> r = new List<T2>();
+            foreach (var i in ll)
+            {
+                r.Add(i.v);
+            }
+            return r;
+        }
+
+        private void Update(T k, T2 v)
+        {
+            var lln = d[k];
+            lln.Value.v = v;
+        }
+
+        private void Promote(T k)
+        {
+            var lln = d[k];
+            ll.Remove(lln);
+            ll.AddFirst(lln);
+        }
+
+        private void Evict()
+        {
+            var lln = ll.Last;
+            ll.Remove(lln);
+            d.Remove(lln.Value.k);
+        }
+    }
+
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache obj = new LRUCache(capacity);
+     * int param_1 = obj.Get(key);
+     * obj.Put(key,value);
+     */
+}
---
+++ LRUCache/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using LRUCache;
+
+namespace LRUCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Demo();
+        }
+
+        public void Demo()
+        {
+            // [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+            var c = new LruCache<int,int>(2);
+            c.Put(1, 1);
+            c.Put(2, 2);
+            c.Get(1);
+            c.Put(3,3);
+            c.Get(2);
+            c.Put(4,4);
+            c.Get(1);
+            c.Get(3);
+            c.Get(4);
+            foreach (var i in c.GetLruList())
+            {
+                Console.Write($"{i},");
+            }
+            Console.WriteLine();
+
+            var c2 = new LruCache<int, string>(3);
+            c2.Put(1, "www.yahoo.com");
+            c2.Put(2, "www.google.com");
+            c2.Get(1);
+            c2.Put(3, "www.cnn.com");
+            c2.Get(2);
+            c2.Put(4, "www.microsoft.com");
+            c2.Get(1);
+            c2.Get(3);
+            c2.Get(4);
+            foreach (var i in c2.GetLruList())
+            {
+                Console.Write($"{i},");
+            }
+            Console.WriteLine();
+        }
+    }
+}
